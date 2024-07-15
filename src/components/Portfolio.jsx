42786d9e651a86cdc6d036fb2dabe9083490594b
damyantjain <damyant9999@gmail.com>
2024-07-15 15:19:53 -0400

    Added basic structure

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,21 @@
 const Portfolio = () => {
-    return (
+  return (
+    <div>
+      {/* Landing View */}
+      <div>
+        {/* About Me */}
         <div>
-            Portfolio
+            <h1>Damyant Jain</h1>
+            <h3>I am a learner/enthusiast/programmer/...</h3>
+            <h5>I love creating products and exploring the tech world, I have 3+ years of work experience...</h5>
         </div>
-    )
-}
+        {/* My photo */}
+        <div>
+            <img src="https://avatars.githubusercontent.com/u/40546663?v=4" alt="Damyant Jain" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
