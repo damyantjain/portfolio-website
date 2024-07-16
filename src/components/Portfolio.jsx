43297337b2c884b4687219471bc1ccd4ca02de50
damyantjain <damyant9999@gmail.com>
2024-07-16 10:31:00 -0400

    Added project template

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,14 @@
 import Intro from "./Intro";
+import Projects from "./Projects";
 
 const Portfolio = () => {
   const adjectives = ["Developer", "Designer", "Creator", "Innovator"];
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
 
   return (
-    <div>
+    <div className="mx-5 md:mx-24 mb-5 md:mb-10 lg:mx-44" >
       <Intro />
+      <Projects />
     </div>
   );
 };
