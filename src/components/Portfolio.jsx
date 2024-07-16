a195d8458b2dc29cfa3b975b95370a40f5d239e5
damyantjain <damyant9999@gmail.com>
2024-07-16 00:07:19 -0400

    Fixed responsivness for mobile

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,16 +1,16 @@
-import profileImage from './../assets/profile.jpg';
+import profileImage from "./../assets/profile.jpg";
 
 const Portfolio = () => {
   const adjectives = ["Developer", "Designer", "Creator", "Innovator"];
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
 
   return (
-    <div>
+    <div className="bg-red-300">
       {/* Landing View */}
       <div className="h-screen bg-[#f0f4f4] flex justify-center items-center">
-        <div className="flex flex-col lg:flex-row mx-24 gap-4 items-center">
+        <div className="flex flex-col lg:flex-row lg:mx-24 lg:gap-4">
           {/* About Me */}
-          <div className="w-full md:w-8/12 m-5 flex flex-col items-center justify-center px-16">
+          <div className="w-full lg:w-8/12 flex flex-col items-center justify-center px-16 pt-10">
             <div className="font-poppins align-middle text-center mb-8 text-6xl font-bold text-[#333333]">
               Damyant Jain
             </div>
@@ -25,9 +25,9 @@ const Portfolio = () => {
             </div>
           </div>
           {/* My photo */}
-          <div className="m-5 w-full lg:w-4/12 flex justify-center items-center">
+          <div className="p-5 w-full lg:w-4/12 flex justify-center items-center">
             <img
-              className="rounded-full drop-shadow-xl object-cover shadow-slate-500"
+              className="rounded-full drop-shadow-xl object-cover shadow-slate-500 h-72 w-72 "
               src={profileImage}
               alt="Damyant Jain"
             />
