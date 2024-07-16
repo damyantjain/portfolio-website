d59cacffc24b69daa2d0fc11e789c154bc39f9a4
damyantjain <damyant9999@gmail.com>
2024-07-16 11:22:04 -0400

    2 template for project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,7 +30,7 @@ const Projects = () => {
     <div className="align-middle text-center font-poppins text-4xl font-bold text-[#333333]">
       <div>My Projects</div>
       {/* List of Projects */}
-      <div className="grid grid-cols-3 gap-2 lg:gap-8 mt-4 lg:mt-20">
+      <div className="grid grid-cols-2 gap-2 lg:gap-8 mt-4 lg:mt-20">
         {projects.map((project, index) => {
           return (
             <div key={index} className="hover:cursor-pointer hover:scale-95 transition-transform duration-500 ease-in-out">
