d27bc300b0242bf577fd3c67a8aeb6d1f13231b7
damyantjain <damyant9999@gmail.com>
2024-07-17 19:59:50 -0400

    Add links to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,34 +1,8 @@
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
+import projects from "../db/projects";
 
 const Projects = () => {
-  const projects = [
-    {
-      title: "Project 1",
-      description: "Description of Project 1",
-      image: "Image URL of Project 1",
-      link: "Link to Project 1",
-    },
-    {
-      title: "Project 2",
-      description: "Description of Project 2",
-      image: "Image URL of Project 2",
-      link: "Link to Project 2",
-    },
-    {
-      title: "Project 3",
-      description: "Description of Project 3",
-      image: "Image URL of Project 3",
-      link: "Link to Project 3",
-    },
-    {
-      title: "Project 4",
-      description: "Description of Project 4",
-      image: "Image URL of Project 4",
-      link: "Link to Project 4",
-    },
-  ];
-
   const { ref, inView } = useInView({
     threshold: 0.3,
     triggerOnce: true,
@@ -52,13 +26,16 @@ const Projects = () => {
     visible: { opacity: 1, y: 0, transition: { duration: 2, ease: "easeOut" } },
   };
 
+  const handleProjectClick = (link) => {
+    window.open(link, "_blank");
+  };
+
   return (
     <div
       ref={ref}
       className="align-middle text-center font-poppins text-4xl font-bold text-[#333333]"
     >
       <div>My Projects</div>
-      {/* List of Projects */}
       <motion.div
         className="grid grid-cols-2 gap-2 lg:gap-8 mt-4 md:mt-20"
         initial="hidden"
@@ -66,9 +43,12 @@ const Projects = () => {
         variants={containerVariants}
       >
         {projects.map((project, index) => (
-          <div className="hover:cursor-pointer hover:scale-95 transition-transform duration-500 ease-in-out transform ">
-            <motion.div key={index} variants={itemVariants}>
-              {/* Project Image */}
+          <div
+            key={index}
+            className="hover:cursor-pointer hover:scale-95 transition-transform duration-500 ease-in-out transform"
+            onClick={() => handleProjectClick(project.link)}
+          >
+            <motion.div variants={itemVariants}>
               <div className="rounded-xl shadow-2xl object-cover bg-gray-200 h-56 w-full lg:h-96">
                 <img src={project.image} alt={project.name} />
               </div>
---
+++ src/db/projects.js
@@ -0,0 +1,47 @@
+const projects = [
+  {
+    title: "Kanbas",
+
+    description:
+      "A robust Learning Management System (LMS) inspired by Canvas, designed to streamline the management of courses, assignments, grades, and modules in an educational setting. ",
+
+    image: "Image URL of Project 1",
+
+    link: "https://github.com/damyantjain/kanbas-react-web-app",
+  },
+
+  {
+    title: "Customer Manager",
+
+    description:
+      "A powerful WPF-based client for managing customer data, leveraging gRPC services for high performance and scalability. Ideal for businesses needing reliable customer management solutions.",
+
+    image: "Image URL of Project 2",
+
+    link: "https://github.com/damyantjain/WPF-Customer-App",
+  },
+
+  {
+    title: "Jiffy",
+
+    description:
+      "A comprehensive food delivery application built with React.js, Redux, and TailwindCSS . Integrates live APIs from Swiggy offering real-time ordering experience.",
+
+    image: "Image URL of Project 3",
+
+    link: "https://github.com/damyantjain/jiffy",
+  },
+
+  {
+    title: "Admitly",
+
+    description:
+      "A unified admission portal for rapid admission process for both students and universities. Powered by Spring Boot and deployed on Azure Web Apps",
+
+    image: "Image URL of Project 4",
+
+    link: "https://github.com/gauravtejwani24/UniversityApplicationPortal",
+  },
+];
+
+export default projects;
