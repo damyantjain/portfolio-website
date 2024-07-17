d0e12273f6a12643512dd187bfdf8aa6858feb3f
damyantjain <damyant9999@gmail.com>
2024-07-17 16:02:06 -0400

    Add projects animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,6 @@
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+
 const Projects = () => {
   const projects = [
     {
@@ -26,27 +29,55 @@ const Projects = () => {
     },
   ];
 
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        staggerChildren: 0.3,
+        duration: 1,
+        ease: "easeOut",
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 2, ease: "easeOut" } },
+  };
+
   return (
-    <div className="align-middle text-center font-poppins text-4xl font-bold text-[#333333]">
+    <div
+      ref={ref}
+      className="align-middle text-center font-poppins text-4xl font-bold text-[#333333]"
+    >
       <div>My Projects</div>
       {/* List of Projects */}
-      <div className="grid grid-cols-2 gap-2 lg:gap-8 mt-4 md:mt-20">
-        {" "}
-        {projects.map((project, index) => {
-          return (
-            <div
-              key={index}
-              className="hover:cursor-pointer hover:scale-95 transition-transform duration-500 ease-in-out"
-            >
+      <motion.div
+        className="grid grid-cols-2 gap-2 lg:gap-8 mt-4 md:mt-20"
+        initial="hidden"
+        animate={inView ? "visible" : "hidden"}
+        variants={containerVariants}
+      >
+        {projects.map((project, index) => (
+          <div className="hover:cursor-pointer hover:scale-95 transition-transform duration-500 ease-in-out transform ">
+            <motion.div key={index} variants={itemVariants}>
               {/* Project Image */}
-              <div className="rounded-xl shadow-2xl object-cover h-56 w-full lg:h-96">
-                <img src={project.image} />
+              <div className="rounded-xl shadow-2xl object-cover bg-gray-200 h-56 w-full lg:h-96">
+                <img src={project.image} alt={project.name} />
               </div>
-            </div>
-          );
-        })}
-      </div>
+            </motion.div>
+          </div>
+        ))}
+      </motion.div>
     </div>
   );
 };
+
 export default Projects;
