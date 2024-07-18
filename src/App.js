daa6b7b4abb4a486352dad77b3620d3a43c570fa
damyantjain <damyant9999@gmail.com>
2024-07-18 16:31:09 -0400

    Basic setup for dark mode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Portfolio from "./components/Portfolio";
 import NavBar from "./components/NavBar";
+import { createContext } from "react";
 
 const AppLayout = () => {
+  const ThemeContext = createContext({
+    darkMode: false,
+  });
+
   return (
-    <div>
-      <NavBar />
-      <Portfolio />
-    </div>
+    <ThemeContext.Provider value={{ darkMode: false }}>
+      <div>
+        <NavBar />
+        <Portfolio />
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-scroll";
-import dj from "./../assets/dj.png";
+import dj from "./../assets/dj_circle.png";
 
 const NavBar = () => {
   const calculateDuration = (target) => {
@@ -12,53 +12,65 @@ const NavBar = () => {
     const minDuration = 400;
     const maxDuration = 2000;
     duration = Math.min(Math.max(duration, minDuration), maxDuration);
-  
+
     return duration;
   };
 
-  return (
-    <nav className="hidden md:flex fixed top-0 left-0 w-full bg-white shadow-lg z-50 justify-center">
-      {/* <img src={dj} className="h-20 w-20" /> */}
-      <div>
-        <ul className="flex font-poppins text-[#333333] items-center space-x-10 h-20">
-          <li className="transistion-colors hover:text-[#9d9d9d]">
-            <Link
-              to="home"
-              smooth="easeInOutQuad"
-              duration={() => calculateDuration("#home")}
-            >
-              Home
-            </Link>
-          </li>
-          <li className="transition-colors hover:text-[#9d9d9d]">
-            <Link
-              to="projects"
-              smooth="easeInOutQuad"
-              duration={() => calculateDuration("#projects")}
-            >
-              Projects
-            </Link>
-          </li>
-          <li className="transition-colors hover:text-[#9d9d9d]">
-            <Link
-              to="resume"
-              smooth="easeInOutQuad"
-              duration={() => calculateDuration("#resume")}
-            >
-              Resume
-            </Link>
-          </li>
+  const toggleDarkMode = () => {};
 
-          <li className="transition-colors hover:text-[#9d9d9d]">
-            <Link
-              to="skills"
-              smooth="easeInOutQuad"
-              duration={() => calculateDuration("#skills")}
-            >
-              Skills
-            </Link>
-          </li>
-        </ul>
+  return (
+    <nav className="hidden items-center fixed top-0 left-0 w-full bg-white shadow-lg z-50 md:flex px-16">
+      <div className="w-full grid grid-cols-3 items-center">
+        <div>
+          <img src={dj} className="h-20 w-20" />
+        </div>
+
+        <div className=" md:flex justify-center">
+          <ul className=" flex font-poppins text-[#333333] items-center space-x-10 h-20">
+            <li className="transition-colors hover:text-[#9d9d9d]">
+              <Link
+                to="home"
+                smooth="easeInOutQuad"
+                duration={() => calculateDuration("#home")}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="transition-colors hover:text-[#9d9d9d]">
+              <Link
+                to="projects"
+                smooth="easeInOutQuad"
+                duration={() => calculateDuration("#projects")}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="transition-colors hover:text-[#9d9d9d]">
+              <Link
+                to="resume"
+                smooth="easeInOutQuad"
+                duration={() => calculateDuration("#resume")}
+              >
+                Resume
+              </Link>
+            </li>
+            <li className="transition-colors hover:text-[#9d9d9d]">
+              <Link
+                to="skills"
+                smooth="easeInOutQuad"
+                duration={() => calculateDuration("#skills")}
+              >
+                Skills
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div
+          className="flex justify-end relative text-xl hover:cursor-pointer"
+          onClick={toggleDarkMode}
+        >
+          ☀
+        </div>
       </div>
     </nav>
   );
