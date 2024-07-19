b4e23ccf06622c13f794524601ae3da33ea9487f
damyantjain <damyant9999@gmail.com>
2024-07-19 01:56:10 -0400

    Added local storage for dark mode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Portfolio from "./components/Portfolio";
 import NavBar from "./components/NavBar";
 import ThemeContext from "./context/ThemeContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const AppLayout = () => {
+  const getInitialMode = () => {
+    return JSON.parse(localStorage.getItem("dark"));
+  }
 
+  const [darkMode, setDarkMode] = useState(getInitialMode());
 
-  const [darkMode, setDarkMode] = useState(false);
+  useEffect(() => {
+    localStorage.setItem("dark", darkMode);
+  }, [darkMode])
 
   return (
     <ThemeContext.Provider value={{ darkMode: darkMode, setDarkMode }}>
-      <div className={`dark:bg-[#121212] ${darkMode? "dark" : ""} `}>
+      <div className={`dark:bg-[#121212] ${darkMode ? "dark" : ""} `}>
         <NavBar />
         <Portfolio />
       </div>
