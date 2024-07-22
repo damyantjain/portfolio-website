ab9eb4664732796c2a419717c68648f5805bdceb
damyantjain <damyant9999@gmail.com>
2024-07-21 22:29:46 -0400

    Added Project Images

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,8 @@
 import { motion } from "framer-motion";
-import kanbasImg from "../assets/kanbas.png";
-import customerManagementImg from "../assets/customerManagement.png";
-import jiffyImg from "../assets/jiffy.png";
-import admitlyImg from "../assets/admitly.png";
+import kanbasImg from "../assets/project/kanbas.png";
+import customerManagementImg from "../assets/project/customerManagement.png";
+import jiffyImg from "../assets/project/jiffy.png";
+import admitlyImg from "../assets/project/admitly.png";
 import { useContext } from "react";
 import ThemeContext from "../context/ThemeContext";
 
@@ -37,15 +37,15 @@ const ProjectCard = ({ project }) => {
         >
           <motion.div variants={itemVariants}>
             <div className="h-80 md:h-96 lg:h-[420px] 2xl:h-[460px]">
-              <div className="relative w-full rounded-xl bg-slate-100 h-48 md:h-64 xl:h-72 2xl:h-[340px]">
+              <div className="relative rounded-xl shadow-lg shadow-[#b3b3b3] dark:shadow-[#1a1919] w-full h-48 md:h-64 xl:h-72 2xl:h-[340px]">
                 <img
-                  className="p-3 w-full h-full rounded-3xl object-cover"
+                  className="w-full h-full rounded-xl  object-cover"
                   src={imageMap[project.image]}
                   alt={project.name}
                 />
   
                 {darkMode && (
-                  <div className="absolute p-3 rounded-xl inset-0 bg-black opacity-10"></div>
+                  <div className="absolute rounded-xl inset-0 bg-black opacity-20"></div>
                 )}
               </div>
               <div className="px-3 md:px-5 justify-start text- items-start">
