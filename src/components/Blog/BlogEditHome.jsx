b7fae9e63b98932a5c1229d015dc013745be4fd3
damyantjain <damyant9999@gmail.com>
2024-07-26 18:05:32 -0400

    Added state to handled blog editing

--- src/components/Blog/BlogEditHome.jsx
+++ src/components/Blog/BlogEditHome.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from "react";
 import { BLOGS_URL } from "../../util/constants";
 import { useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { addBlog } from "../../store/blogSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addBlog, updateDescription, updateTitle } from "../../store/blogSlice";
 
 const BlogEditHome = () => {
   const { id } = useParams();
-  const [blog, setBlog] = useState({ title: "", description: "" });
-  const [editedBlog, setEditedBlog] = useState({ title: "", description: "" });
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -21,10 +19,10 @@ const BlogEditHome = () => {
       return;
     }
     var data = await response.json();
-    setBlog(data);
-    setEditedBlog(data);
     dispatch(addBlog(data));
   };
+
+  const editedBlog = useSelector((state) => state.blog);
 
   return (
     <div className="mx-5 md:mx-16 lg:mx-32 xl:mx-44">
@@ -35,9 +33,7 @@ const BlogEditHome = () => {
           <input
             type="text"
             value={editedBlog?.title}
-            onChange={(e) =>
-              setEditedBlog({ ...editedBlog, title: e.target.value })
-            }
+            onChange={(e) => dispatch(updateTitle(e.target.value))}
             className="border-2 border-gray-300 p-2 w-full"
           />
         </div>
@@ -47,9 +43,7 @@ const BlogEditHome = () => {
           <textarea
             type="text"
             value={editedBlog?.description}
-            onChange={(e) =>
-              setEditedBlog({ ...editedBlog, description: e.target.value })
-            }
+            onChange={(e) => dispatch(updateDescription(e.target.value))}
             className="border-2 border-gray-300 p-2 w-full"
           />
         </div>
--- src/store/blogSlice.js
+++ src/store/blogSlice.js
@@ -1,15 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const blogSlice = createSlice({
-    name: "blogSlice",
-    initialState: {
-        blog: {}
+  name: "blogSlice",
+  initialState: {
+    blog: { title: "", description: "" },
+    editedBlog: { title: "", description: "" },
+  },
+  reducers: {
+    addBlog: (state, action) => {
+      state.blog = action.payload;
+      state.editedBlog = action.payload;
     },
-    reducers: {
-        addBlog: (state, action) => {
-            state.blog = action.payload;
-        }
-    }
-})
-export const { addBlog } = blogSlice.actions;
+    updateTitle: (state, action) => {
+      state.editedBlog.title = action.payload;
+    },
+    updateDescription: (state, action) => {
+      state.editedBlog.description = action.payload;
+    },
+  },
+});
+export const { addBlog, updateTitle, updateDescription } = blogSlice.actions;
 export default blogSlice.reducer;
