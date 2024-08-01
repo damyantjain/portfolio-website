97715f339e78da776f5204564e584bd45ce53597
damyantjain <damyant9999@gmail.com>
2024-08-01 11:28:55 -0400

    Added drag and drop for blog editor

--- src/components/Blog/BlogEditor/ContentBlock.jsx
+++ src/components/Blog/BlogEditor/ContentBlock.jsx
@@ -1,25 +1,56 @@
 import TextBlock from "./TextBlock";
 import ImageBlock from "./ImageBlock";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { reorderBlocks } from "../../../store/blogSlice";
 
 const ContentBlock = () => {
   const editedBlog = useSelector((state) => state.blog.editedBlog);
+  const dispatch = useDispatch();
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const reorderedBlocks = Array.from(editedBlog.contentBlocks);
+    const [movedBlock] = reorderedBlocks.splice(result.source.index, 1);
+    reorderedBlocks.splice(result.destination.index, 0, movedBlock);
+
+    dispatch(reorderBlocks(reorderedBlocks));
+  };
 
   return (
-    <div>
-      {editedBlog?.contentBlocks?.map((contentBlock, index) => (
-        <div
-          key={index}
-          className="mx-auto px-4 w-full  dark:bg-[#2222222d] rounded-xl p-6 border border-gray-200 dark:border-0 mb-6"
-        >
-          {contentBlock?.type === "text" ? (
-            <TextBlock contentBlock={contentBlock} />
-          ) : (
-            <ImageBlock contentBlock={contentBlock} />
-          )}
-        </div>
-      ))}
-    </div>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="contentBlocks">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {editedBlog?.contentBlocks?.map((contentBlock, index) => (
+              <Draggable
+                key={contentBlock._id}
+                draggableId={contentBlock._id}
+                index={index}
+              >
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className="mx-auto px-4 w-full dark:bg-[#2222222d] rounded-xl p-6 border border-gray-200 dark:border-0 mb-6"
+                  >
+                    {contentBlock?.type === "text" ? (
+                      <TextBlock contentBlock={contentBlock} />
+                    ) : (
+                      <ImageBlock contentBlock={contentBlock} />
+                    )}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
+
 export default ContentBlock;
