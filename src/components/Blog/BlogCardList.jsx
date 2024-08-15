52c21a522db5f401f1ad1948b563efd15bb14ef0
damyantjain <damyant9999@gmail.com>
2024-08-15 10:10:35 -0400

    Blog card to sort by latest date

--- src/components/Blog/BlogCardList.jsx
+++ src/components/Blog/BlogCardList.jsx
@@ -1,9 +1,12 @@
 import BlogCard from "./BlogCard";
 
 const BlogCardList = ({ posts }) => {
+
+  const sortedPosts = posts.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+
   return (
     <div className="pb-56">
-      {posts.map((post) => {
+      {sortedPosts.map((post) => {
         return (
           <div key={post?._id} className="pt-10">
             <BlogCard post={post} />
